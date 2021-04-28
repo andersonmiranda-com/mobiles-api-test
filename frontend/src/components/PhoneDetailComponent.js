e8703565c20f831ba6150548a8934aa06415bc85
Anderson Miranda <me@andersonmiranda.com>
2021-04-28 16:43:36 +0200

    edited modal styles

--- frontend/src/components/PhoneDetailComponent.js
+++ frontend/src/components/PhoneDetailComponent.js
@@ -1,21 +1,55 @@
-import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogActions from "@material-ui/core/DialogActions";
 import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
+import CloseIcon from "@material-ui/icons/Close";
+
+const useStyles = makeStyles({
+  root: {
+    width: 400,
+  },
+  media: {
+    maxHeight: 300,
+  },
+  closeButton: {
+    position: "absolute",
+    right: 2,
+    top: 2,
+  },
+});
 
 function PhoneDetailComponent(props) {
   const { detail, onClose } = props;
+  const classes = useStyles();
   return (
     <>
-      <DialogTitle>{detail.title}</DialogTitle>
-      <DialogContent>{detail.description}</DialogContent>
-      <DialogActions>
-        <Button autoFocus onClick={onClose} color="primary">
-          Close
-        </Button>
-      </DialogActions>
+      <DialogTitle className={classes.root}>
+        {detail.title}
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <Box display="flex" justifyContent="center">
+          <img
+            alt={detail.title}
+            className={classes.media}
+            src={detail.image}
+          />
+        </Box>
+        <p>{detail.description}</p>
+        <p>Color: {detail.color}</p>
+        <p>Memory: {detail.memory} GB</p>
+        <p>Manufacturer: {detail.manufacturer}</p>
+        <Box display="flex" justifyContent="flex-end">
+          <h2>{detail.price}, €</h2>
+        </Box>
+      </DialogContent>
     </>
   );
 }
